0681e1b0f83cd90a97df307b90a9b723871d4327
Logan Ellis <mrloganellis@gmail.com>
2019-10-08 12:05:59 -0400

    got the image slider down! need to fix hamburger menu, some styling, and make a landing page

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 
 
-function menuClick() {
+$(document).ready(function menuClick() {
     $('#hamburger').on('click', '#hamburgerMenu', function(event) {
         alert('hey!');
 
         event.preventDefault();
-        $('.hamburgerMenu').css('display', 'inline');
+        $('.hamburgerMenu').removeClass('hide');
     });
-}
-
-function initialize() {
-    menuClick();
-}
+});
 
 
-$(initialize)
+
+
+$(document).ready(function() {
+    $('.next').on('click', function(m) {
+        m.preventDefault();
+        let currentImg = $('.active');
+        let nextImg = currentImg.next();
+
+        if ( nextImg.length) {
+            currentImg.removeClass('active').css('z-indez', -10);
+            nextImg.addClass('active').css('z-index', -10);
+        }
+    });
+
+    $('.prev').on('click', function (p) {
+        p.preventDefault();
+        let currentImg = $('.active');
+        let prevImg = currentImg.prev();
+
+        if (prevImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', -10);
+        }
+    });
+});
