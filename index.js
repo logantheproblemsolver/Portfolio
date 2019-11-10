1fed11a4469352c8e13fe443147bf50b2df397b1
Logan Ellis <mrloganellis@gmail.com>
2019-11-10 09:08:06 -0500

    made my portfolio mobile responsive!

--- index.js
+++ index.js
@@ -3,35 +3,7 @@
 $(document).ready(function menuClick() {
     $('#hamburger').on('click', function(event) {
         event.preventDefault();
-        $('.hamburgerMenu').toggleClass('hide');
-        $('#hamburger').toggleClass('hide');
-        $('#hamburger').toggleClass('menuShow');
+        $('.menu').toggleClass('hide');
     });
 });
 
-
-
-
-$(document).ready(function() {
-    $('.next').on('click', function(m) {
-        m.preventDefault();
-        let currentImg = $('.active');
-        let nextImg = currentImg.next();
-
-        if ( nextImg.length) {
-            currentImg.removeClass('active').css('z-indez', -10);
-            nextImg.addClass('active').css('z-index', -10);
-        }
-    });
-
-    $('.prev').on('click', function (p) {
-        p.preventDefault();
-        let currentImg = $('.active');
-        let prevImg = currentImg.prev();
-
-        if (prevImg.length) {
-            currentImg.removeClass('active').css('z-index', -10);
-            prevImg.addClass('active').css('z-index', -10);
-        }
-    });
-});
