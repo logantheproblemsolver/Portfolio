fdbf99ad06b6c92878b18161aa4f12d5d9271dd7
Logan Ellis <mrloganellis@gmail.com>
2019-11-20 22:13:53 -0500

    fixed mobile responsiveness and hamburger menu

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ $(document).ready(function menuClick() {
     $('#hamburger').on('click', function(event) {
         event.preventDefault();
         $('.menu').toggleClass('hide');
+        $('.hamburger').toggleClass('onClick');
     });
 });
 
